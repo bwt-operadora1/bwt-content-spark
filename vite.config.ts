f7d68cdefb4fea495344f1ffff66150def5757f5
bwt-operadora1 <gabriel.cordeiro@bwtoperadora.com.br>
2026-04-09 17:52:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
     allowedHosts: ["conteudo.bwtoperadora.com.br"],
-    hmr: mode === "development" ? { overlay: false } : false,
+    hmr: false,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
